83df23e4226df91189f0b93744f4e37cfbc59c8f
Viktor dias <61331026+viktordias@users.noreply.github.com>
2023-03-15 19:49:45 -0300

    BackEnd da aplicação para execução via terminal

---
+++ Codigos/FAPERJ.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+int main(int argc, char *argv[]) {
+
+        //DadosEntrada dado =	Ler("/home/viktor/Documentos/GitHub/IT_FAPERJ_2020/IT_FAPERJ_2020 /datafiler/Dados2.txt");
+	//DadosEntrada dado =	Ler("C:/Users/odair/Documents/GitHub/IT_FAPERJ_2020/IT_FAPERJ_2020_Windows/datafiler/Dados2.txt");
+        //GETDD2Dserial(dado);
+
+	printf("\n Agora versao paralela \n");
+
+        //GETDD2Dparalelo(dado);
+
+
+	return 0;
+
+}
